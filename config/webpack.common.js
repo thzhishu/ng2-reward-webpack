41b85cab479189b523a73859b619f4d88d13093c
ToBePlus <float1101@gmail.com>
2016-06-12 09:48:01 +0800

    图片根目录

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -188,7 +188,7 @@ module.exports = {
             // },
             {
                 test: /\.(scss|sass)$/,
-                loader: 'raw-loader!style-loader!css-loader!autoprefixer-loader!sass'
+                loader: 'raw-loader!style-loader!css-loader!autoprefixer-loader!sass-loader'
             },
 
         ]
